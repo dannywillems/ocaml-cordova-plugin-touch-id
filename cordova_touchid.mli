78eef4fb3d00f0c00f413ceb434e0e3afc9422b3
Danny Willems <contact@danny-willems.be>
2016-06-17 22:24:54 +0200

    Use functions instead of objects

--- cordova_touchid.mli
+++ cordova_touchid.mli
@@ -1,25 +1,14 @@
 (* -------------------------------------------------------------------------- *)
-class touchid : Ojs.t ->
-  object
-    inherit Ojs.obj
-    (* ---------------------------------------------------------------------- *)
-    method authenticate   : (unit -> unit)    -> (* success_cb *)
-                            (unit -> unit)    -> (* error_cb if authentication
-                                                    fails *)
-                            string            -> (* text explaining why the
-                                                    application needs access *)
-                            unit
-    (* ---------------------------------------------------------------------- *)
+val authenticate :
+  (unit -> unit)    -> (* success_cb *)
+  (unit -> unit)    -> (* error_cb if authentication fails *)
+  string            -> (* text explaining why the application needs access *)
+  unit
+[@@js.global "touchid.authenticate"]
 
-    (* ---------------------------------------------------------------------- *)
-    method check_support  : (unit -> unit)    -> (* success_cb *)
-                            (string -> unit)  -> (* error_cb *)
-                            unit
-    (* ---------------------------------------------------------------------- *)
-  end
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> touchid
-[@@js.get "touchid"]
+val check_support :
+  (unit -> unit)    -> (* success_cb *)
+  (string -> unit)  -> (* error_cb *)
+  unit
+[@@js.global "touchid.checkSupport"]
 (* -------------------------------------------------------------------------- *)
